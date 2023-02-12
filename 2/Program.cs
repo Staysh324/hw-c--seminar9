94660c5967475a08ca03cde7b5191982e0c2ba2f
Artem <proceller@yandex.ru>
2023-02-12 18:56:23 +0300

    end work

---
+++ 2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int InputInt(string message) // метод ввода числа
+{
+    Console.Write(message + ":");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+
+
+int SumNumbers(int min, int max) // рекурсивный метод сложения чисел от min до max
+{
+    if (max == 0)
+    {
+        return 0;
+    }
+    return max + SumNumbers(min, max - 1);
+
+}
+
+
+int min = InputInt("введитме минимальный порог");
+int max = InputInt("введите максимальный порог");
+if (min > max)
+{
+    System.Console.WriteLine("минимальное число не может быть больше максимального");
+}
+else
+{
+    int sum = SumNumbers(min, max);
+    System.Console.WriteLine($"сумма чисел от {min} до {max} = {sum}");
+}
+
---
+++ 3/Program.cs
@@ -0,0 +1,30 @@
+﻿int InputInt(string message) // метод ввода числа
+{
+    Console.Write(message + ":");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+
+int AkkernanFunction(int n, int m) // рекурсивный метод вычисления функции Аккермана
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    if (n > 0 && m == 0)
+    {
+        return AkkernanFunction(n - 1, 1);
+    }
+    if (n > 0 && m > 0)
+    {
+        return AkkernanFunction(n - 1, AkkernanFunction(n, m - 1));
+    }
+    return AkkernanFunction(n, m);
+}
+
+
+int n = InputInt("введите n");
+int m = InputInt("введите m");
+System.Console.WriteLine($"результатом для функции Аккермана со значениями A({n},{m}) = {AkkernanFunction(n, m)}");
