fe7d0c996466c65498b86029557e1fdd0bc5e52f
Artem <proceller@yandex.ru>
2023-02-13 14:26:52 +0300

    rework 2

---
+++ 1-1/Program.cs
@@ -0,0 +1,47 @@
+﻿//благодаря этой задаче я понял, что Вы объясняли на семинаре в первой задаче! =)
+
+
+int InputInt(string message) // метод ввода числа
+{
+    Console.Write(message + ":");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+
+void ShowEvenNumbers(int min, int max) // рекурсивный метод вывода чётных чисел от введенного минимума до максимума
+{
+    if (max == 0)
+    {
+        return;
+    }
+
+    if (max % 2 != 0)
+    {
+        max -= 1;
+    }
+    g
+    ShowEvenNumbers(min, max - 2);
+    System.Console.Write($"{max}; ");
+}
+
+
+int min = InputInt("введите минимальный порог");
+int max = InputInt("введите максимальный порог");
+
+if (min > max)
+{
+    System.Console.WriteLine("минимальное число не может быть больше максимального");
+    Environment.Exit(1);
+}
+
+if (max == 0 | max == 1)
+{
+    System.Console.WriteLine("в заданном диапазоне четных чисел нет");
+}
+else
+{
+    System.Console.WriteLine($"чётные числа от {min} до {max}:");
+    ShowEvenNumbers(min, max);
+}
